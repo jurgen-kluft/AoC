63e26fc0844cac463690a4590e411572edbfbd48
Jurgen Kluft <jurgen@mbp.local>
2016-01-01 11:46:08 +0800

    day9 done..

--- day9/main.go
+++ day9/main.go
@@ -72,96 +72,170 @@ type traversal struct {
 	maximum int
 }
 
-func back(self traversal) bool {
-	if self.length == 0 {
+func (t *traversal) back() bool {
+	if t.length == 0 {
 		return false
 	}
 
-	// travel backwards, remove the current node from 'visited'
-	// reduce the travelled length
-	current := self.route[self.length]
-	delete(self.visited, current.name)
-	self.length--
+	// travel backwards, remove the current node from the route
+	// reduce the route length and remove the node from 'visited'
+	t.length--
+	current := t.route[t.length]
+	delete(t.visited, current.name)
+
+	if t.length == 0 {
+		return false
+	}
 
 	// re-initialize the direction of the now current node
-	current = self.route[self.length]
-	self.dir = self.visited[current.name]
+	current = t.route[t.length-1]
+	t.dir = t.visited[current.name]
 
-	return self.dir < len(current.roads)
+	return t.dir < len(current.roads)
 }
 
-func empty(self traversal) bool {
-	return self.length == 0
-}
-
-func complete(self traversal) bool {
-	return self.length == self.maximum
-}
-
-func distance(self traversal) int {
-	distance := 0
-	if self.length > 0 {
-		current := self.route[0]
-		for p, n := range self.route {
-			if p > 0 {
-				distance += current.distance[n.name]
-				current = n
-			}
-		}
+func (t *traversal) forward() bool {
+	// No need to travel when the route is complete or empty
+	if t.complete() || t.empty() {
+		return false
 	}
-	return distance
-}
-
-func forward(self traversal) bool {
-	current := self.route[self.length]
-	for self.dir < len(current.roads) {
-		next := current.roads[self.dir]
-		if _, visited := self.visited[next.name]; !visited {
-			self.visited[current.name] = self.dir + 1
-			self.visited[next.name] = 0
-			self.dir = 0
-			self.length++
-			self.route[self.length] = next
+	current := t.route[t.length-1]
+	for t.dir < len(current.roads) {
+		next := current.roads[t.dir]
+		if _, visited := t.visited[next.name]; !visited {
+			// Remember the next direction we should travel on current node
+			t.visited[current.name] = t.dir + 1
+			// Initialize the direction of the new node to the first direction
+			t.dir = 0
+			t.visited[next.name] = 0
+			t.route[t.length] = next
+			t.length++
 			return true
 		}
+		// Try next dir to see if we can travel into that direction
+		t.dir++
 	}
 
 	return false
 }
 
-func addRoute(from string, to string, distance int, graph map[string]*node) {
-	var nfrom = new(node)
-	var nto = new(node)
-	var exists = false
-	if nfrom, exists = graph[from]; !exists {
-		nfrom = &node{name: from, roads: make([]*node, 0), distance: make(map[string]int)}
-		graph[from] = nfrom
+func (t *traversal) start(origin *node, maximum int) {
+	t.visited = make(map[string]int, maximum)
+	t.route = make([]*node, maximum, maximum)
+	t.length = 1
+	t.maximum = maximum
+	t.route[0] = origin
+	t.dir = 0
+}
+
+func (t *traversal) empty() bool {
+	return t.length == 0
+}
+
+func (t *traversal) complete() bool {
+	return t.length == t.maximum
+}
+
+func (t *traversal) distance() int {
+	distance := 0
+	fmt.Printf("%s", t.route[0].name)
+	for i := 1; i < t.length; i++ {
+		from := t.route[i-1]
+		to := t.route[i]
+		d := from.distance[to.name]
+		fmt.Printf(" -> %d,%s", d, to.name)
+		distance += d
 	}
-	if nto, exists = graph[to]; !exists {
-		nto = &node{name: to, roads: make([]*node, 0), distance: make(map[string]int)}
-		graph[to] = nto
-	}
-	nfrom.roads = append(nfrom.roads, nto)
-	nfrom.distance[to] = distance
+	fmt.Printf(" = %v\n", distance)
+	return distance
 }
 
 func computeShortestRoute(graph map[string]*node) (distanceOfShortestRoute int) {
-	for originName, originNode := range graph {
-		traverse := new(traversal)
-		for traverse.complete == false && traverse.
+
+	// Set shortest distance to a very high number
+	distanceOfShortestRoute = 1 * 1024 * 1024 * 1024
+
+	totalNumberOfRoutes := 0
+
+	traverse := new(traversal)
+	for _, originNode := range graph {
+		traverse.start(originNode, len(graph))
+		// Try all possible routes from this origin
+		for traverse.empty() == false {
+
+			// Travel forward until we have a complete route
+			for traverse.complete() == false {
+				if traverse.forward() == false {
+					break
+				}
+			}
+
+			// If traveling forward got us a complete route
+			// then compute the distance and see if this is
+			// now the shortest
+
+			if traverse.complete() {
+				distance := traverse.distance()
+				totalNumberOfRoutes++
+				if distance < distanceOfShortestRoute {
+					distanceOfShortestRoute = distance
+				}
+			}
+
+			// Travel backwards on the route until we find a node that still
+			// has directions that we can travel on
+			for traverse.back() == false {
+				if traverse.empty() {
+					break
+				}
+			}
+		}
 	}
+	fmt.Printf("Traveled %v number of routes\n", totalNumberOfRoutes)
 	return
 }
 
+func addRoute(from string, to string, distance int, graph map[string]*node) {
+	if nfrom, exists := graph[from]; !exists {
+		nfrom = &node{name: from, roads: make([]*node, 0, 8), distance: make(map[string]int)}
+		graph[from] = nfrom
+	}
+	if nto, exists := graph[to]; !exists {
+		nto = &node{name: to, roads: make([]*node, 0, 8), distance: make(map[string]int)}
+		graph[to] = nto
+	}
+
+	nfrom := graph[from]
+	nto := graph[to]
+
+	nfrom.roads = append(nfrom.roads, nto)
+	nfrom.distance[to] = distance
+
+	nto.roads = append(nto.roads, nfrom)
+	nto.distance[from] = distance
+}
+
 func findShortestRouteFromFile(filename string) (distanceOfShortestRoute int) {
-	graph := make(map[string]*node)
+	graph := make(map[string]*node, 64)
 
 	computator := func(text string, line int) {
 		from, to, distance := decodeRoute(text)
-		fmt.Printf("From %s to %s, distance %v\n", from, to, distance)
+		//fmt.Printf("From %s to %s, distance %v\n", from, to, distance)
 		addRoute(from, to, distance, graph)
 	}
 	iterateOverLinesInTextFile(filename, computator)
+
+	printgraph := false
+	if printgraph {
+		for nodeName, nodeObject := range graph {
+			fmt.Printf("origin: %s, roads", nodeName)
+			for _, nodeTo := range nodeObject.roads {
+				distance := nodeObject.distance[nodeTo.name]
+				fmt.Printf(": %s(%d)", nodeTo.name, distance)
+			}
+			fmt.Printf("\n")
+		}
+	}
 
 	distanceOfShortestRoute = computeShortestRoute(graph)
 	return
@@ -169,5 +243,5 @@ func findShortestRouteFromFile(filename string) (distanceOfShortestRoute int) {
 
 func main() {
 	distanceOfShortestRoute := findShortestRouteFromFile("input.text")
-	fmt.Printf("The shortest route is: %v", distanceOfShortestRoute)
+	fmt.Printf("The shortest route is: %v\n", distanceOfShortestRoute)
 }
